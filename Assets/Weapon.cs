446bf2ef6bd9ae49e5b60940ef1fe77acbd9ef00
tiin1705 <daonhattiin1705@gmail.com>
2024-01-30 22:23:26 +0700

    Attack
    bản thô

---
+++ Assets/Weapon.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Debug.Log("Attack");
+            anim.SetBool("Attack", true);
+        }
+        else
+            anim.SetBool("Attack", false);
+
+    }
+}
