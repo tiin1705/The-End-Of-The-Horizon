6a114817b95b57b6dec5f445f59d01f7d70057a0
Quangtv208 <vingquang20082003@gmail.com>
2024-03-06 02:01:58 +0700

    update menu system

--- Assets/Resources/Scripts/LoginSystem.cs
+++ Assets/Resources/Scripts/LoginSystem.cs
@@ -52,8 +52,8 @@ public class LoginSystem : MonoBehaviour
 
     }
     public void LoginJohn()
-    { 
-        //
+    {
+        SceneManager.LoadScene(0);
     }
     public void LoginCancel()
     {
