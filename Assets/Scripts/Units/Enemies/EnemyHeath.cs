ee3a5f15186691194dcf7741be6695c2e91402ce
Thien <127167498+yume14@users.noreply.github.com>
2024-04-08 22:45:46 +0700

    Fix Enemy

--- Assets/Scripts/Units/Enemies/EnemyHeath.cs
+++ Assets/Scripts/Units/Enemies/EnemyHeath.cs
@@ -22,30 +22,26 @@ public class EnemyHeath : MonoBehaviour
     {
         currentHealth = staringHealth;
         animator = GetComponent<Animator>();
-       
 
-}
 
-public void TakeDamage(int damage)
+    }
+
+    public void TakeDamage(int damage)
     {
         currentHealth -= damage;
         Debug.Log(currentHealth);
-        knockBack.GetKnockedBack(PlayerController.Instance.transform,7f);
+        knockBack.GetKnockedBack(PlayerController.Instance.transform, 10f);
         StartCoroutine(flash.FlashRoutine());
     }
 
     public void DetectDeath()
     {
-        if (currentHealth <= 0) {
-            StartCoroutine(DestroyAfterAnimation());
+        if (currentHealth <= 0)
+        {
             animator.SetTrigger("Death");
-            enemyPathfinding.GetComponent<EnemyPathfinding>().SetDead(true);
+            Destroy(gameObject);
 
         }
     }
-    private IEnumerator DestroyAfterAnimation()
-    {
-        yield return new WaitForSeconds(deathAnimationLength);
-        Destroy(gameObject);
-    }
+
 }
