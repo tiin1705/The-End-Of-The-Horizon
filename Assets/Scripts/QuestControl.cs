87ecc7b93a1e0ef576bf8fb30beb653e08091005
Thien <127167498+yume14@users.noreply.github.com>
2024-05-02 23:30:47 +0700

    Fix Collider

--- Assets/Scripts/QuestControl.cs
+++ Assets/Scripts/QuestControl.cs
@@ -5,12 +5,26 @@ using UnityEngine.UI;
 
 public class QuestControl : MonoBehaviour
 {
-    public GameObject NPCChat;
+    public GameObject NPCChat1;
     public GameObject NPCChat2;
-    public Button next;
-    public GameObject QuestBorder;
-    public GameObject Choose;
-    public Button YesBtn;
-    public Button NoBtn;
+    public GameObject NPCChat3;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("NPC"))
+        {
+            NPCChat1.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("NPC"))
+        {
+            NPCChat1.SetActive(false);
+            NPCChat2.SetActive(false);
+            NPCChat3.SetActive(false);
+
+        }
+    }
+
 
 }
