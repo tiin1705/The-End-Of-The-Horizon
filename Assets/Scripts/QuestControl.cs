9681c003f5ecaeeb948d4857c813b27fa3d81243
Thien <127167498+yume14@users.noreply.github.com>
2024-05-02 22:14:40 +0700

    up

---
+++ Assets/Scripts/QuestControl.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestControl : MonoBehaviour
+{
+    public GameObject NPCChat;
+    public GameObject NPCChat2;
+    public Button next;
+    public GameObject QuestBorder;
+    public GameObject Choose;
+    public Button YesBtn;
+    public Button NoBtn;
+
+}
