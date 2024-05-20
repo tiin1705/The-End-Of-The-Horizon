1d6bce3efefae1e23f2e351bb0801ac66931ab52
tiin1705 <74749243+tiin1705@users.noreply.github.com>
2024-05-21 02:10:57 +0700

    update

--- Assets/Scripts/Management/Singleton.cs
+++ Assets/Scripts/Management/Singleton.cs
@@ -1,34 +1,25 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class Singleton<T> : MonoBehaviour where T : Singleton<T>
 {
     private static T instance;
-    public static T Instance
-    {
-        get
-        {
-            if (instance == null)
-            {
-                instance = FindObjectOfType<T>();
-                if (instance == null)
-                {
-                    Debug.LogError($"No instance of {typeof(T)} found in the scene.");
-                }
-            }
-            return instance;
-        }
-    }
+    public static T Instance { get { return instance; } }
 
     protected virtual void Awake()
     {
-        if (instance != null && instance != this)
+        if (instance != null && this.gameObject != null)
         {
-            Destroy(gameObject);
+            Destroy(this.gameObject);
         }
         else
         {
             instance = (T)this;
+        }
+
+        if (!gameObject.transform.parent)
+        {
             DontDestroyOnLoad(gameObject);
         }
     }
